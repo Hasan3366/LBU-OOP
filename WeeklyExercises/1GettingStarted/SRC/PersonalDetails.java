d67711b360d454391b59f53261d3a652f2cecc0c
c3662471 <H@M>
2024-01-29 12:14:15 +0000

    PersonalDetails

---
+++ WeeklyExercises/1GettingStarted/SRC/PersonalDetails.java
@@ -0,0 +1,9 @@
+public class PersonalDetails {
+
+
+    public static void main (String[] argv) {
+        System.out.print("Name: Hasan Miah Address: 23 maple street Age: 21 Phone number: 07761915224");
+    }
+
+}
+
